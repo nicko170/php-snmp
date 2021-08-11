2fdb0be8e1afa68bac9ae267e8cb29c1e840091f
nicko170 <nick@npratley.net>
2021-08-12 01:31:54 +1000

    fix copyright

--- src/MIBS/HasMIBS.php
+++ src/MIBS/HasMIBS.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * PHP SNMP - A fluent interface to SNMP network devices
+ * PHP SNMP - A fluent interface to SNMP network devices.
  *
  * @link      https://github.com/nicko170/php-snmp
  * @author    Nick Pratley <nick@npratley.net>
  * @licence   https://github.com/nicko170/php-snmp/blob/main/LICENSE.md
  * @copyright Copyright (c) 2021 - 2021 Nick Pratley - (https://devlan.io)
- *
  */
 
 namespace Nicko170\PhpSnmp\MIBS;
